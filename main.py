fc0dd4b34ecb5c83106c85e542e5d26b9917b5a9
Lucknay <160678001+Lucknay@users.noreply.github.com>
2024-11-07 16:19:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+dicionario1 = dict()
+dicionario2= dict()
+dicionario3= dict()
+
+nome = input('Digite o nome para adicionar:')
+email = input('Digite o e-mail para adicionar:')
+telefone = int(input('Digite o telefone para adicionar:'))
+
+
+dicionario1['nome'] = nome
+dicionario1['email'] = email
+dicionario1['telefone'] = telefone
+
+print(dicionario1, dicionario2, dicionario3)
